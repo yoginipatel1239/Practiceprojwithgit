cef5195e4819c6b319e11e6db0541fd9e8a010c7
yogini.patel@archesoftronix.com <yogini.patel@archesoftronix.com>
2022-02-23 16:14:02 +0530

    Initial Commit

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+
+            int i , j;
+            int x = 5;
+            int y = 5;
+            int length = 9;
+            for ( i = 1; i <= length; i++)
+            {
+                Console.WriteLine(i);
+
+                for ( j = 2; j <= length; j++)
+                {                    
+                    Console.WriteLine(i + ""+ j);
+                   
+                }
+            }
+        }
+    }
+}
